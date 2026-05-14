9921f77fd040363d2c58a95f3c9f6fa737434a1e
sreerajjayaraj <31250143+sreerajjayaraj@users.noreply.github.com>
2026-05-14 14:19:10 +0400

    Delete player-status-modifier-v1.05.01-RVA-FIXED-fix39 directory

--- player-status-modifier-v1.05.01-RVA-FIXED-fix39/src/runtime/durability_logic.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <cstdint>
-
-bool TryAdjustDurabilityDelta(uintptr_t entry, uint16_t current_value, int16_t* delta);
-bool TryAdjustDurabilityWrite(uintptr_t entry, uint16_t* value);
